f639ec93c62a25e428a3dc400bbecfc183617457
Jaicke Santos <jaicke-santos@outlook.com>
2022-01-16 18:36:23 -0300

    feat: Adds notifications system

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,21 +1,37 @@
 var NOTIFICATIONS = (function(notifications){
   'use strict'
 
+  var readNotification = function(notification){
+    let notificationId = notification.find('input').val()
+    let namespace = location.pathname.includes('student') ? 'student' : 'teacher'
+
+    $.ajax({
+      url: `${location.origin}/${namespace}/notifications/${notificationId}/read`,
+      type: 'put',
+    })
+  }
+
   var markNotificationAsRead = function(){
     $('body').on('click', '.notification-card', function(){
-      let notificationId = $(this).find('input').val()
-      let namespace = location.pathname.includes('student') ? 'student' : 'teacher'
-
-      $.ajax({
-        url: `${location.origin}/${namespace}/notifications/${notificationId}/read`,
-        type: 'put',
-      })
+      readNotification($(this))
     })
+  }
+
+  var markAllAsRead = function(){
+    $('.notification-card').each(function(){
+      readNotification($(this))
+    });
+  }
+
+  var removeNewNotificationAlert = function() {
+    $('#new-notification-alert').html('')
   }
 
   var notifications = {
     init: function(){
       markNotificationAsRead()
+      markAllAsRead()
+      removeNewNotificationAlert()
     }
   }
 
---
+++ app/jobs/notification_relay_job.rb
@@ -0,0 +1,12 @@
+class NotificationRelayJob < ApplicationJob
+  queue_as :default
+
+  def perform(notification)
+    html = ApplicationController.render partial: "layouts/shared/new_notification", locals: { notification: notification }, formats: [:html]
+
+    namespace = notification.recipient_type == 'Student::User' ? 'student' : 'teacher'
+
+    html_to_notification_on_menu = ApplicationController.render partial: "#{namespace}/notifications/#{notification.notifiable_type.underscore}/#{notification.action}", locals: { notification: notification }, formats: [:html]
+    ActionCable.server.broadcast "notifications:#{notification.recipient_id}", html: html, html_to_notification_on_menu: html_to_notification_on_menu, notifications_not_read_count: notification.recipient.notifications.not_read.count
+  end
+end
